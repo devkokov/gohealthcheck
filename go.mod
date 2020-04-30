ed8c78e63b5a3c4bf0600a6895659785409d02cf
devkokov <dm.kokov@gmail.com>
2020-04-30 15:30:20 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/devkokov/health-check
+
+go 1.14
---
+++ health_check.go
@@ -0,0 +1,20 @@
+package health_check
+
+import (
+	"flag"
+	"net/http"
+	"os"
+)
+
+func Register(addr string) {
+	hc := flag.Bool("hc", false, "perform a health check")
+	flag.Parse()
+	if !*hc {
+		return
+	}
+	resp, err := http.Get(addr)
+	if err != nil || resp.StatusCode != 200 {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
